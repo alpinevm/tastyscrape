09946e4429d80e9d8dc8bae9ee5895185e983d4c
Cliff Syner <c4syner@gmail.com>
2021-10-24 12:53:05 -0500

    Refactor #4

--- tastyscrape/example.py
+++ tastyscrape/example.py
@@ -1,11 +1,8 @@
 import asyncio
-import logging
 
 from tastyscrape.bases import option_chain, underlying
 from tastyscrape.bases.session import TastyAPISession
 from tastyscrape.bases.streamer import DataStreamer
-LOGGER = logging.getLogger(__name__)
-
 
 async def main_loop(session: TastyAPISession, streamer: DataStreamer):
     sub_values = {
@@ -15,23 +12,22 @@ async def main_loop(session: TastyAPISession, streamer: DataStreamer):
     undl = underlying.Underlying('SPY')
 
     chain = await option_chain.get_option_chain(session, undl)
-    LOGGER.info('Chain strikes: %s', chain.get_all_strikes())
 
     await streamer.add_data_sub(sub_values)
 
     async for item in streamer.listen():
-        LOGGER.info('Received item: %s' % item.data)
+        print(item.data)
 
 def main():
-    tasty_client = TastyAPISession("c4synerInvest", "CliffInvest7890!")
+    tasty_client = TastyAPISession("", "")
     streamer = DataStreamer(tasty_client)
-    LOGGER.info('Streamer token: %s' % streamer.get_streamer_token())
+    print(f'Streaming Token: {streamer.get_streamer_token()}')
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main_loop(tasty_client, streamer))
 
     """
     except Exception:
-        LOGGER.exception('Exception in main loop')
+        .exception('Exception in main loop')
     finally:
         # find all futures/tasks still running and wait for them to finish
         pending_tasks = [
